9cc8512ba66637468834c2efc93b1001e308f02f
npardal <nicolepardall@gmail.com>
2023-04-01 19:41:41 -0400

    updated ui

--- BeGreen/BeGreen/Views/Home/HomePage.swift
+++ BeGreen/BeGreen/Views/Home/HomePage.swift
@@ -11,27 +11,112 @@ struct HomePage: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack{
-                VStack(alignment: .center){
-                    Text("BeGreel")
-                        .multilineTextAlignment(.center)
-                        .font(.custom("AvenirLTStd-Roman", size: 17))
-                        .padding(.top, 20)
+                ScrollView{
                     
-                    HStack {
-                        Text("Morning, Nicole👋")
-                            .font(.custom("AvenirLTStd-Black", size: 33))
-                            .frame(maxWidth:.infinity, alignment: .leading)
+                    VStack(alignment: .center){
+                        Text("BeGreel")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("AvenirLTStd-Roman", size: 17))
                             .padding(.top, 20)
-                            .padding(.leading, 20)
                         
-                        Spacer()
-                    }
-                   
+                        HStack {
+                            Text("Morning, Nicole👋")
+                                .font(.custom("AvenirLTStd-Black", size: 33))
+                                .frame(maxWidth:.infinity, alignment: .leading)
+                                .padding(.top, 100)
+                                .padding(.leading, 20)
+                                .padding(.bottom, 30)
+                      
+                        }
+                        
+                        VStack{
+                            HStack{
+                                Text("Today's suggestion")
+                                    .font(.custom("AvenirLTStd-Black", size: 20))
+                                    .frame(maxWidth:.infinity, alignment: .leading)
+                                    .padding(.leading, 20)
+                                    .padding(.top, 5)
+                                
+                                Spacer()
+                                
+                                ZStack {
+                                            RoundedRectangle(cornerRadius: 20)
+                                        .fill(CustomColor.yellow)
+                                            Text("Upload")
+                                                .foregroundColor(.white)
+                                    .font(.custom("AvenirLTStd-Roman", size: 16))                                        }
+                                        .frame(width: 100, height: 35)
+                                        .padding(.trailing, 15)
+                            }
+                            RoundedRectangle(cornerRadius: 40)
+                                        .fill(Color.gray.opacity(0.2))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 40)
+                                                .stroke(Color.yellow, lineWidth: 2)
+                                        )
+                                        .frame(height: 70)
+                                        .padding()
+                                        .overlay(
+                                            HStack(spacing: 10) {
+                                                Image("idea")
+                                                    .resizable()
+                                                    .frame(width: 40, height: 40)
+                                                Text("Pick up some trash on the ground")
+                                                    .font(.custom("AvenirLTStd-Roman", size: 15))
+                                                    .foregroundColor(.black)
+                                                    .font(.system(size: 14))
+                                                    .padding(10)
+                                            }
+                                            .padding(20)
+                                        )
+                        }
+                        
                     
-                    Spacer()
-                }.frame(width: geometry.size.width, height: geometry.size.height)
-                    .background(CustomColor.background)
-            }
+                        ZStack{
+                            
+                            VStack{
+                                HStack{
+                                    ZStack{
+    
+                                        Circle()
+                                            .fill(CustomColor.turquoise)
+                                        Image("sapling")
+                                            .resizable()
+                                            .frame(width: 40, height: 40)
+                                    }.padding(.leading, 20)
+                                    Text("@KanyeQuest ")
+                                        .font(.custom("AvenirLTStd-Roman", size: 17))
+                                    Spacer()
+                                    Text("18 mins ago")
+                                        .font(.custom("AvenirLTStd-Roman", size: 12))
+                                        .foregroundColor(.gray)
+                                        .padding(.trailing, 20)
+                                }
+                                Image("man")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 350, height: 300)
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .stroke(CustomColor.turquoise, lineWidth: 6)
+                                    )
+                                
+                             
+                            }
+                            
+                        }
+                        
+                        
+                     
+                    }.frame(width: geometry.size.width, height: geometry.size.height)
+                        .background(CustomColor.background)
+                        .ignoresSafeArea()
+                }
+            }.frame(width: geometry.size.width)
+                .background(CustomColor.background)
+                .ignoresSafeArea()
+                
         }
     }
 }
--- BeGreen/BeGreen/Views/Profile/Profile.swift
+++ BeGreen/BeGreen/Views/Profile/Profile.swift
@@ -9,12 +9,226 @@ import SwiftUI
 
 struct Profile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            VStack(spacing: 25){
+                Spacer()
+                Image("anusha")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 250, height: 250)
+                    .clipped()
+                    .cornerRadius(2500)
+                    .overlay(
+                            Circle()
+                                .stroke(CustomColor.yellow, lineWidth: 5)
+                        )
+                    .padding(.bottom, 10)
+                    
+                
+                Text("Nathan Gasole")
+                    .foregroundColor(.black)
+                    .font(.custom("AvenirLTStd-Black", size: 21))
+                
+                Text("I love helping the environment all 🍀")
+                    .font(.custom("AvenirLTStd-Roman", size: 15))
+                    .foregroundColor(.black)
+                    .padding(.bottom, 20)
+                
+                CategoryView(followers: 300, following: 12, points: 39)
+                
+                Text("Achievements")
+                    .font(.custom("AvenirLTStd-Black", size: 20))
+                    .frame(maxWidth:.infinity, alignment: .leading)
+          
+                    .padding(.leading, 20)
+                   
+          
+                VStack {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 25)
+                            .foregroundColor(CustomColor.turquoise)
+                            .opacity(0.1)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .opacity(0.1)
+                            )
+                        HStack {
+                            Image("flame")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            
+                            VStack(alignment: .leading){
+                                Text("1 day Streak!")
+                                    .foregroundColor( Color.white)
+                                    .font(.custom("AvenirLTStd-Black", size: 25))
+                                
+                                    .padding(.bottom, 0.1)
+                                
+                                
+                                Text("Congratulations, collect your reward!")
+                                    .multilineTextAlignment(.leading)
+                                
+                                    .foregroundColor( Color.black)
+                                    .font(.custom("AvenirLTStd-Book", size: 11))
+                                    .lineSpacing(3)
+                            }
+                        }}
+                    
+                }
+                .frame(width: 350, height: 100)
+                
+                
+                
+               
+                
+                //MVVM -> model (data - users) viewmodel(business logic - adding users) view: initial view everytime the data changes
+                
+                //clas observable object that changes
+                //user ID & password
+                //observe the changes ->
+                
+                
+                
+            
+                
+            } .frame(width: geometry.size.width, height: geometry.size.height)
+                .background(CustomColor.background)
+        }
     }
 }
 
 struct Profile_Previews: PreviewProvider {
     static var previews: some View {
-        Profile()
+        GeometryReader { geometry in
+            VStack(spacing: 25){
+                Spacer()
+                Image("anusha")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 250, height: 250)
+                    .clipped()
+                    .cornerRadius(2500)
+                    .overlay(
+                            Circle()
+                                .stroke(CustomColor.yellow, lineWidth: 5)
+                        )
+                    .padding(.bottom, 10)
+                    
+                
+                Text("Nathan Gasole")
+                    .foregroundColor(.black)
+                    .font(.custom("AvenirLTStd-Black", size: 21))
+                
+                Text("I love helping the environment all 🍀")
+                    .font(.custom("AvenirLTStd-Roman", size: 15))
+                    .foregroundColor(.black)
+                    .padding(.bottom, 20)
+                
+                CategoryView(followers: 300, following: 12, points: 39)
+                
+                Text("Achievements")
+                    .font(.custom("AvenirLTStd-Black", size: 20))
+                    .frame(maxWidth:.infinity, alignment: .leading)
+          
+                    .padding(.leading, 20)
+                   
+          
+                VStack {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 25)
+                            .foregroundColor(CustomColor.turquoise)
+                            .opacity(0.1)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .opacity(0.1)
+                            )
+                        HStack {
+                            Image("flame")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            
+                            VStack(alignment: .leading){
+                                Text("1 day Streak!")
+                                    .foregroundColor( Color.white)
+                                    .font(.custom("AvenirLTStd-Black", size: 25))
+                                
+                                    .padding(.bottom, 0.1)
+                                
+                                
+                                Text("Congratulations, collect your reward!")
+                                    .multilineTextAlignment(.leading)
+                                
+                                    .foregroundColor( Color.black)
+                                    .font(.custom("AvenirLTStd-Book", size: 11))
+                                    .lineSpacing(3)
+                            }
+                        }}
+                    
+                }
+                .frame(width: 350, height: 100)
+                
+                
+                
+               
+                
+                //MVVM -> model (data - users) viewmodel(business logic - adding users) view: initial view everytime the data changes
+                
+                //clas observable object that changes
+                //user ID & password
+                //observe the changes ->
+                
+                
+                
+            
+                
+            } .frame(width: geometry.size.width, height: geometry.size.height)
+                .background(CustomColor.background)
+        }
+    }
+}
+
+
+
+struct CategoryView: View {
+    var followers: Int
+    var following: Int
+    var points: Int
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Text("Followers")
+                   
+                    .font(.custom("AvenirLTStd-Book", size: 15))
+                    .padding(.bottom, 1)
+                Text("\(followers)")
+                    .font(.custom("AvenirLTStd-Roman", size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(CustomColor.turquoise)
+            }
+            Spacer()
+            VStack {
+                Text("Following")
+             
+                    .font(.custom("AvenirLTStd-Book", size: 15))
+                    .padding(.bottom, 2)
+                Text("\(following)")
+                    .font(.custom("AvenirLTStd-Roman", size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(CustomColor.turquoise)
+            }
+            Spacer()
+            VStack {
+                Text("Points")
+                    .font(.custom("AvenirLTStd-Book", size: 15))
+                    .padding(.bottom, 2)
+                Text("\(points)")
+                    .font(.custom("AvenirLTStd-Roman", size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(CustomColor.turquoise)
+            }
+        }
+        .padding()
+        .padding([.leading, .trailing], 18)
     }
 }
